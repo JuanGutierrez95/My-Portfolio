187f86feb06e5f4d4976099051d65c7d5075892a
ale9516 <juan.gutierrez19958@gmail.com>
2023-07-26 21:45:26 -0300

    commit

--- components/ResumeContent.tsx
+++ components/ResumeContent.tsx
@@ -22,8 +22,8 @@ const ResumeContent = () => {
             {shortText}
             {fullText}
             <a
-              href="\assets\Front-End-FullStackDeveloper_Juan_Gutierrez.pdf"
-              download="Front-End-FullStackDeveloper_Juan_Gutierrez.pdf"
+              href="\assets\Front-End-FullStack-Developer_Juan_Gutierrez.pdf"
+              download="Front-End-FullStack-Developer_Juan_Gutierrez.pdf"
               aria-label="Resume"
             >
               <button className="text-blue">
